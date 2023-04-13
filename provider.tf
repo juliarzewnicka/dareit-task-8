abf00b32c2fab092e9aa44af990294863f0f568b
juliarzewnicka <124995537+juliarzewnicka@users.noreply.github.com>
2023-04-14 01:47:18 +0200

    fix: add kubernetes provider

--- provider.tf
+++ provider.tf
@@ -3,3 +3,14 @@ provider "google" {
   region  = "us-central1"
   zone    = "us-central1-c"
 }
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "my-context"
+}
+
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "my-first-namespace"
+  }
+}
