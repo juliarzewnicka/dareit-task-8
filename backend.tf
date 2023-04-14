f5f0d34a84252d61687baae11dd42de5bd55cb12
juliarzewnicka <124995537+juliarzewnicka@users.noreply.github.com>
2023-04-14 02:06:50 +0200

    fix: change provider version

--- backend.tf
+++ backend.tf
@@ -7,11 +7,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.32.0"
+      version = "4.61.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "4.32.0"
+      version = "4.61.0"
     }
   }
 }
